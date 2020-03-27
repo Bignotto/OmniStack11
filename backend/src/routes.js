836072d0d0c0a2b5504374fe55841369589528f0
Thiago Bignotto <bignotto@gmail.com>
2020-03-27 17:03:31 -0300

    Aula2: Produtor CREATE e LIST prontos

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const Produtor = require("./controllers/ProdutorController");
 
 const routes = express.Router();
 
 routes.get("/", (req, res) => {
   res.send("Semana OmniStack 11 - Thiago Bignotto");
 });
+
+routes.post("/produtor", Produtor.create);
+routes.get("/produtor", Produtor.index);
 
 module.exports = routes;
