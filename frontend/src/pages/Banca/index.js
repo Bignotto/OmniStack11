a5189e69554253adcac06af52016e23e45b18abb
Thiago Bignotto <bignotto@gmail.com>
2020-03-29 19:23:08 -0300

    Aula3: Pagina Banca listagem e delete prontos

--- frontend/src/pages/Banca/index.js
+++ frontend/src/pages/Banca/index.js
@@ -1,17 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FiPower, FiTrash2 } from "react-icons/fi";
+
+import api from "../../services/api";
 
 import logoImg from "../../assets/logo.svg";
 
 import "./styles.css";
 
 export default function Banca() {
+  const [produtos, setProdutos] = useState([]);
+
+  const nomeProdutor = localStorage.getItem("nome");
+  const idProdutor = localStorage.getItem("id");
+
+  useEffect(() => {
+    api
+      .get("banca", {
+        headers: {
+          Authorization: idProdutor
+        }
+      })
+      .then(response => {
+        setProdutos(response.data);
+      });
+  }, [idProdutor]);
+
+  async function handelDelete(id) {
+    try {
+      await api.delete(`produtos/${id}`, {
+        headers: {
+          Authorization: idProdutor
+        }
+      });
+      setProdutos(produtos.filter(produto => produto.id !== id));
+    } catch (error) {
+      alert("Deu erro. Tente novamente!");
+    }
+  }
+
   return (
     <div className="banca-container">
       <header>
         <img src={logoImg} alt="Produtores Unidos!" />
-        <span>Bem vindo Thiago</span>
+        <span>Bem vindo {nomeProdutor}</span>
         <Link className="button" to="/produto/novo">
           Novo produto
         </Link>
@@ -19,52 +51,28 @@ export default function Banca() {
           <FiPower size={18} color="#E02041" />
         </button>
       </header>
+
       <h1>Produtos cadastrados</h1>
+
       <ul>
-        <li>
-          <strong>Produto:</strong>
-          <p>Nome do produto</p>
-          <strong>Descrição:</strong>
-          <p>Descrição teste</p>
-          <strong>Preço:</strong>
-          <p>R$ 199,90</p>
-          <button type="button">
-            <FiTrash2 size={20} color="#A8A8B3" />
-          </button>
-        </li>
-        <li>
-          <strong>Produto:</strong>
-          <p>Nome do produto</p>
-          <strong>Descrição:</strong>
-          <p>Descrição teste</p>
-          <strong>Preço:</strong>
-          <p>R$ 199,90</p>
-          <button type="button">
-            <FiTrash2 size={20} color="#A8A8B3" />
-          </button>
-        </li>
-        <li>
-          <strong>Produto:</strong>
-          <p>Nome do produto</p>
-          <strong>Descrição:</strong>
-          <p>Descrição teste</p>
-          <strong>Preço:</strong>
-          <p>R$ 199,90</p>
-          <button type="button">
-            <FiTrash2 size={20} color="#A8A8B3" />
-          </button>
-        </li>
-        <li>
-          <strong>Produto:</strong>
-          <p>Nome do produto</p>
-          <strong>Descrição:</strong>
-          <p>Descrição teste</p>
-          <strong>Preço:</strong>
-          <p>R$ 199,90</p>
-          <button type="button">
-            <FiTrash2 size={20} color="#A8A8B3" />
-          </button>
-        </li>
+        {produtos.map(produto => (
+          <li key={produto.id}>
+            <strong>Produto:</strong>
+            <p>{produto.nome}</p>
+            <strong>Descrição:</strong>
+            <p>{produto.descricao}</p>
+            <strong>Preço:</strong>
+            <p>
+              {Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL"
+              }).format(produto.valor)}
+            </p>
+            <button type="button" onClick={() => handelDelete(produto.id)}>
+              <FiTrash2 size={20} color="#A8A8B3" />
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
--- frontend/src/pages/Registro/index.js
+++ frontend/src/pages/Registro/index.js
@@ -1,12 +1,44 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
+
+import api from "../../services/api";
 
 import logoImg from "../../assets/logo.svg";
 
 import "./styles.css";
 
 export default function Registro() {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [endereco, setEndereco] = useState("");
+  const [cidade, setCidade] = useState("");
+  const [uf, setUf] = useState("");
+
+  const history = useHistory();
+
+  async function handleRegistro(e) {
+    e.preventDefault();
+
+    const data = {
+      nome,
+      email,
+      telefone,
+      endereco,
+      cidade,
+      uf
+    };
+
+    try {
+      const response = await api.post("produtor", data);
+      alert(`Seu ID de acesso ${response.data.id}`);
+      history.push("/");
+    } catch (error) {
+      alert("Deu erro, tente de novo!");
+    }
+  }
+
   return (
     <div className="register-container">
       <div className="content">
@@ -21,14 +53,41 @@ export default function Registro() {
             Voltar
           </Link>
         </section>
-        <form>
-          <input placeholder="Seu nome" />
-          <input placeholder="E-Mail" type="email" />
-          <input placeholder="Telefone" />
+        <form onSubmit={handleRegistro}>
+          <input
+            placeholder="Seu nome"
+            value={nome}
+            onChange={e => setNome(e.target.value)}
+          />
+          <input
+            placeholder="E-Mail"
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <input
+            placeholder="Telefone"
+            value={telefone}
+            onChange={e => setTelefone(e.target.value)}
+          />
+          <input
+            placeholder="Endereço"
+            value={endereco}
+            onChange={e => setEndereco(e.target.value)}
+          />
 
           <div className="input-group">
-            <input placeholder="Cidade" />
-            <input placeholder="UF" style={{ width: 80 }} />
+            <input
+              placeholder="Cidade"
+              value={cidade}
+              onChange={e => setCidade(e.target.value)}
+            />
+            <input
+              placeholder="UF"
+              style={{ width: 80 }}
+              value={uf}
+              onChange={e => setUf(e.target.value)}
+            />
           </div>
           <button className="button" type="submit">
             Cadastrar
